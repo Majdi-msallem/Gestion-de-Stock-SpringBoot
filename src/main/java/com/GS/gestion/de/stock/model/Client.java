526786c33e7e021d963f42e636e952316e9cd5cb
Majdi-msallem <majdi.msallem@esprit.tn>
2023-01-16 15:51:20 +0100

    Article client category adresse

--- src/main/java/com/GS/gestion/de/stock/model/Client.java
+++ src/main/java/com/GS/gestion/de/stock/model/Client.java
@@ -1,15 +1,39 @@
 package com.GS.gestion.de.stock.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name= "client")
 public class Client extends AbstractEntity {
+    @Column(name ="nom")
+    private String nom;
+
+    @Column(name ="prenom")
+    private String prenom;
+
+
+    @Embedded
+     private Adresse adresse;
+
+    @Column(name ="photo")
+    private String photo;
+
+    @Column(name ="mail")
+    private String mail;
+
+    @Column(name ="numTel")
+    private String numTel;
+
+    @OneToMany(mappedBy = "client")
+    private List<CommandeClient> commandelient;
 }
