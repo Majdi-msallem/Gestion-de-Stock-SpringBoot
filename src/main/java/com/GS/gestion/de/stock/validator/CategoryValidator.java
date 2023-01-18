f9b7c471dcf9a20982bd9717313642c0ec8db4c5
Majdi-msallem <majdi.msallem@esprit.tn>
2023-01-18 22:19:36 +0100

    Validators

---
+++ src/main/java/com/GS/gestion/de/stock/validator/CategoryValidator.java
@@ -0,0 +1,19 @@
+package com.GS.gestion.de.stock.validator;
+
+import com.GS.gestion.de.stock.dto.CategoryDto;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryValidator {
+    public static List<String> validate(CategoryDto categoryDto) {
+        List<String> errors = new ArrayList<>();
+
+        if (categoryDto == null || !StringUtils.hasLength(categoryDto.getCode())) {
+            errors.add("Veuillez renseigner le code de la categorie");
+        }
+        return errors;
+    }
+
+}
