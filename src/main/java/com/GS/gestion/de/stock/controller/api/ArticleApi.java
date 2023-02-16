4b39674a4a457f3f03ccbb9eacbd8cbc2902022e
Majdi-msallem <majdi.msallem@esprit.tn>
2023-02-16 15:59:59 +0100

    ArticleApi-Constants

--- src/main/java/com/GS/gestion/de/stock/controller/api/ArticleApi.java
+++ src/main/java/com/GS/gestion/de/stock/controller/api/ArticleApi.java
@@ -1,17 +1,26 @@
 package com.GS.gestion.de.stock.controller.api;
 
 import com.GS.gestion.de.stock.dto.ArticleDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 import static com.GS.gestion.de.stock.utils.Constants.APP_ROOT;
-
+@Api("articles")
 public interface ArticleApi {
 
 
     @PostMapping(value = APP_ROOT + "/articles/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Enregistrer un article", notes = "Cette methode permet d'enregistrer ou modifier un article", response = ArticleDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "L'objet article cree / modifie"),
+            @ApiResponse(code = 400, message = "L'objet article n'est pas valide")
+    })
     ArticleDto save(@RequestBody ArticleDto dto);
 
     @GetMapping(value = APP_ROOT + "/articles/{idArticle}", produces = MediaType.APPLICATION_JSON_VALUE)
