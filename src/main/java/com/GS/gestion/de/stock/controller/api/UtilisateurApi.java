9f93f190c989f38b05473e3ad3a3568f09cf8822
Majdi-msallem <majdi.msallem@esprit.tn>
2023-02-18 15:36:10 +0100

    Utilisateurs,Ventes API-Controller

---
+++ src/main/java/com/GS/gestion/de/stock/controller/api/UtilisateurApi.java
@@ -0,0 +1,30 @@
+package com.GS.gestion.de.stock.controller.api;
+
+import com.GS.gestion.de.stock.dto.UtilisateurDto;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.GS.gestion.de.stock.utils.Constants.UTILISATEUR_ENDPOINT;
+
+@Api("utilisateurs")
+public interface UtilisateurApi {
+    @PostMapping(UTILISATEUR_ENDPOINT + "/create")
+    UtilisateurDto save(@RequestBody UtilisateurDto dto);
+
+//    @PostMapping(UTILISATEUR_ENDPOINT + "/update/password")
+//    UtilisateurDto changerMotDePasse(@RequestBody ChangerMotDePasseUtilisateurDto dto);
+
+    @GetMapping(UTILISATEUR_ENDPOINT + "/{idUtilisateur}")
+    UtilisateurDto findById(@PathVariable("idUtilisateur") Integer id);
+
+    @GetMapping(UTILISATEUR_ENDPOINT + "/find/{email}")
+    UtilisateurDto findByEmail(@PathVariable("email") String email);
+
+    @GetMapping(UTILISATEUR_ENDPOINT + "/all")
+    List<UtilisateurDto> findAll();
+
+    @DeleteMapping(UTILISATEUR_ENDPOINT + "/delete/{idUtilisateur}")
+    void delete(@PathVariable("idUtilisateur") Integer id);
+}
