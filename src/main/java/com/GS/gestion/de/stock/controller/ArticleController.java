dc5f6789b8320382120173491fd7375097c08b23
Majdi-msallem <majdi.msallem@esprit.tn>
2023-02-18 10:33:56 +0100

    Article controller

--- src/main/java/com/GS/gestion/de/stock/controller/ArticleController.java
+++ src/main/java/com/GS/gestion/de/stock/controller/ArticleController.java
@@ -1,7 +1,48 @@
 package com.GS.gestion.de.stock.controller;
 
+
+import com.GS.gestion.de.stock.controller.api.ArticleApi;
+import com.GS.gestion.de.stock.dto.ArticleDto;
+import com.GS.gestion.de.stock.services.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
 @RestController
-public class ArticleController {
+public class ArticleController implements ArticleApi {
+    private ArticleService articleService;
+
+    @Autowired
+    public ArticleController(
+            ArticleService articleService
+    ) {
+        this.articleService = articleService;
+    }
+    @Override
+    public ArticleDto save(ArticleDto dto) {
+        return articleService.save(dto);
+    }
+
+    @Override
+    public ArticleDto findById(Integer id) {
+        return articleService.findById(id);
+    }
+
+    @Override
+    public ArticleDto findByCodeArticle(String codeArticle) {
+        return articleService.findByCodeArticle(codeArticle);
+    }
+
+    @Override
+    public List<ArticleDto> findAll() {
+        return articleService.findAll();
+    }
+
+    @Override
+    public void delete(Integer id) {
+        articleService.delete(id);
+
+    }
 }
